7b170ffa5601d1e19c5552a13cd0f1fcea4be344
Ian Kirkpatrick <ianmann56@gmail.com>
2017-02-27 17:59:05 -0500

    Start initializing crap

---
+++ a4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+}
+
+void halfaddr(int a, int b, int *sum, int *outcary);
+
+void fulladdr(int a, int b, int incary, int *sum, int *outcary);
